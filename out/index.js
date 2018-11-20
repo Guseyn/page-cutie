535523765e3e220c810c9a519189fca3969446c0
gismayilov <guseynism@gmail.com>
2018-11-20 17:42:31 +0300

    implementation

---
+++ out/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  AsyncObject: require('./AsyncObject'),
+  Event: require('./Event'),
+  as: require('./As'),
+  NullError: require('./NullError')
+};
---
+++ src/index.es6.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  AsyncObject: require('./AsyncObject'),
+  Event: require('./Event'),
+  as: require('./As'),
+  NullError: require('./NullError')
+
+}
